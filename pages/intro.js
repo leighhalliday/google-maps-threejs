729b33d1774487805670ade281d4cb2b91db326c
Leigh Halliday <leighhalliday@gmail.com>
2022-12-20 08:27:31 -0500

    Moving overlay outside of React

--- pages/intro.js
+++ pages/intro.js
@@ -27,85 +27,80 @@ export default function App() {
 }
 
 function MyMap() {
-  const [map, setMap] = useState();
+  const [_map, setMap] = useState();
   const ref = useRef();
 
   useEffect(() => {
-    setMap(new window.google.maps.Map(ref.current, mapOptions));
+    const instance = new window.google.maps.Map(ref.current, mapOptions);
+    setMap(instance);
+    createOverlay(instance);
   }, []);
 
-  return (
-    <>
-      <div ref={ref} id="map" />
-      {map && <Overlay map={map} />}
-    </>
-  );
+  return <div ref={ref} id="map" />;
 }
 
-function Overlay({ map }) {
-  useEffect(() => {
-    const overlay = new google.maps.WebGLOverlayView();
-    const loader = new GLTFLoader();
-    let renderer, scene, camera;
+function createOverlay(map) {
+  const overlay = new google.maps.WebGLOverlayView();
+  const loader = new GLTFLoader();
+  let renderer, scene, camera;
 
-    overlay.onAdd = () => {
-      scene = new Scene();
-      camera = new PerspectiveCamera();
-      const light = new AmbientLight(0xffffff, 0.9);
-      scene.add(light);
+  overlay.onAdd = () => {
+    scene = new Scene();
+    camera = new PerspectiveCamera();
+    const light = new AmbientLight(0xffffff, 0.9);
+    scene.add(light);
 
-      // This work is based on "Low Poly Scooter" (https://sketchfab.com/3d-models/low-poly-scooter-cf0b53fddb5c469b9d0259104151f72d) by Qbo2Qbo (https://sketchfab.com/Qbo2Qbo) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
-      loader.loadAsync("/low_poly_scooter/scene.gltf").then((object) => {
-        const group = object.scene;
-        group.scale.setScalar(25);
-        group.rotation.set(Math.PI / 2, 0, 0);
-        group.position.setZ(-120);
-        scene.add(group);
-      });
-    };
+    // This work is based on "Low Poly Scooter" (https://sketchfab.com/3d-models/low-poly-scooter-cf0b53fddb5c469b9d0259104151f72d) by Qbo2Qbo (https://sketchfab.com/Qbo2Qbo) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+    loader.loadAsync("/low_poly_scooter/scene.gltf").then((object) => {
+      const group = object.scene;
+      group.scale.setScalar(25);
+      group.rotation.set(Math.PI / 2, 0, 0);
+      group.position.setZ(-120);
+      scene.add(group);
+    });
+  };
 
-    overlay.onContextRestored = ({ gl }) => {
-      renderer = new WebGLRenderer({
-        canvas: gl.canvas,
-        context: gl,
-        ...gl.getContextAttributes(),
-      });
-      renderer.autoClear = false;
+  overlay.onContextRestored = ({ gl }) => {
+    renderer = new WebGLRenderer({
+      canvas: gl.canvas,
+      context: gl,
+      ...gl.getContextAttributes(),
+    });
+    renderer.autoClear = false;
 
-      loader.manager.onLoad = () => {
-        renderer.setAnimationLoop(() => {
-          map.moveCamera({
-            tilt: mapOptions.tilt,
-            heading: mapOptions.heading,
-            zoom: mapOptions.zoom,
-          });
-
-          if (mapOptions.tilt < 60) {
-            mapOptions.tilt += 0.5;
-          } else if (mapOptions.zoom < 20) {
-            mapOptions.zoom += 0.05;
-          } else if (mapOptions.heading < 125) {
-            mapOptions.heading += 0.5;
-          } else {
-            renderer.setAnimationLoop(null);
-          }
+    loader.manager.onLoad = () => {
+      renderer.setAnimationLoop(() => {
+        map.moveCamera({
+          tilt: mapOptions.tilt,
+          heading: mapOptions.heading,
+          zoom: mapOptions.zoom,
         });
-      };
-    };
 
-    overlay.onDraw = ({ transformer }) => {
-      const matrix = transformer.fromLatLngAltitude({
-        lat: mapOptions.center.lat,
-        lng: mapOptions.center.lng,
-        altitude: 120,
+        if (mapOptions.tilt < 60) {
+          mapOptions.tilt += 0.5;
+        } else if (mapOptions.zoom < 20) {
+          mapOptions.zoom += 0.05;
+        } else if (mapOptions.heading < 125) {
+          mapOptions.heading += 0.5;
+        } else {
+          renderer.setAnimationLoop(null);
+        }
       });
-      camera.projectionMatrix = new Matrix4().fromArray(matrix);
-
-      overlay.requestRedraw();
-      renderer.render(scene, camera);
-      renderer.resetState();
     };
+  };
 
-    overlay.setMap(map);
-  }, [map]);
+  overlay.onDraw = ({ transformer }) => {
+    const matrix = transformer.fromLatLngAltitude({
+      lat: mapOptions.center.lat,
+      lng: mapOptions.center.lng,
+      altitude: 120,
+    });
+    camera.projectionMatrix = new Matrix4().fromArray(matrix);
+
+    overlay.requestRedraw();
+    renderer.render(scene, camera);
+    renderer.resetState();
+  };
+
+  overlay.setMap(map);
 }
